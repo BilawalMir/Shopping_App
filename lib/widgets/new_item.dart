ca3a1aba6bbd8ac42f9fe0c37c73abecf5665abe
BilawalMir <bilawalijaz2019@gmail.com>
2025-08-02 21:05:55 +0500

    update

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -17,6 +17,8 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      print(_enteredName);
+      print(_enteredQuantity);
     }
   }
 
